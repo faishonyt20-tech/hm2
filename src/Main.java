32114373ccbf281b54eb9f8be7a2e319cc0897f3
Nikita <git@github.com:faishonyt20-tech/helloworld.git>
2026-01-26 11:23:54 +0300

    commit hm9

--- src/Main.java
+++ src/Main.java
@@ -3,93 +3,90 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         System.out.println("=== Задача 1 ===");
-        // Задача 1: Создание массивов
-        // 1. Целочисленный массив с помощью ключевого слова new
-        int[] intArray = new int[3];
-        intArray[0] = 1;
-        intArray[1] = 2;
-        intArray[2] = 3;
+        // Задача 1: Расчет статистики выплат авторам
+        int[] inputArray1 = {12000, 8500, 15000, 9200, 11300};
 
-        // 2. Массив дробных чисел, сразу заполненный значениями
-        double[] doubleArray = {1.57, 7.654, 9.986};
+        // Расчет статистики
+        int sum = 0;
+        int max = inputArray1[0];
+        int min = inputArray1[0];
 
-        // 3. Произвольный массив
-        String[] stringArray = {"Java", "Python", "C++", "JavaScript"};
+        for (int payment : inputArray1) {
+            sum += payment;
+            if (payment > max) {
+                max = payment;
+            }
+            if (payment < min) {
+                min = payment;
+            }
+        }
 
-        System.out.println("Массивы созданы:");
-        System.out.println("intArray: " + Arrays.toString(intArray));
-        System.out.println("doubleArray: " + Arrays.toString(doubleArray));
-        System.out.println("stringArray: " + Arrays.toString(stringArray));
+        double average = (double) sum / inputArray1.length;
+        double[] outputArray1 = {sum, max, min, average};
+
+        System.out.println("inputArray1: " + Arrays.toString(inputArray1));
+        System.out.println("outputArray1: " + Arrays.toString(outputArray1));
+        System.out.println("Формат: [сумма, максимум, минимум, среднее]");
 
         System.out.println("\n=== Задача 2 ===");
-        // Задача 2: Вывод элементов по порядку
-        System.out.println("Элементы intArray по порядку:");
-        for (int i = 0; i < intArray.length; i++) {
-            if (i > 0) {
-                System.out.print(", ");
-            }
-            System.out.print(intArray[i]);
-        }
-        System.out.println();
+        // Задача 2: Расчет налога 13% для выплат
+        int[] inputArray2 = {50000, 75000, 45000, 60000, 80000};
+        double[] outputArray2 = new double[inputArray2.length];
 
-        System.out.println("Элементы doubleArray по порядку:");
-        for (int i = 0; i < doubleArray.length; i++) {
-            if (i > 0) {
-                System.out.print(", ");
-            }
-            System.out.print(doubleArray[i]);
+        for (int i = 0; i < inputArray2.length; i++) {
+            outputArray2[i] = inputArray2[i] * 0.13;
         }
-        System.out.println();
 
-        System.out.println("Элементы stringArray по порядку:");
-        for (int i = 0; i < stringArray.length; i++) {
-            if (i > 0) {
-                System.out.print(", ");
-            }
-            System.out.print(stringArray[i]);
-        }
-        System.out.println();
+        System.out.println("inputArray2: " + Arrays.toString(inputArray2));
+        System.out.println("outputArray2: " + Arrays.toString(outputArray2));
+        System.out.println("Формат: [налог для каждой выплаты]");
 
         System.out.println("\n=== Задача 3 ===");
-        // Задача 3: Вывод элементов в обратном порядке
-        System.out.println("Элементы intArray в обратном порядке:");
-        for (int i = intArray.length - 1; i >= 0; i--) {
-            if (i < intArray.length - 1) {
-                System.out.print(", ");
-            }
-            System.out.print(intArray[i]);
-        }
-        System.out.println();
+        // Задача 3: Поиск бонусов выше 5000 ₽
+        int[] inputArray3 = {3500, 6200, 4800, 7500, 5500};
+        boolean[] outputArray3 = new boolean[inputArray3.length];
 
-        System.out.println("Элементы doubleArray в обратном порядке:");
-        for (int i = doubleArray.length - 1; i >= 0; i--) {
-            if (i < doubleArray.length - 1) {
-                System.out.print(", ");
-            }
-            System.out.print(doubleArray[i]);
+        for (int i = 0; i < inputArray3.length; i++) {
+            outputArray3[i] = inputArray3[i] > 5000;
         }
-        System.out.println();
 
-        System.out.println("Элементы stringArray в обратном порядке:");
-        for (int i = stringArray.length - 1; i >= 0; i--) {
-            if (i < stringArray.length - 1) {
-                System.out.print(", ");
-            }
-            System.out.print(stringArray[i]);
-        }
-        System.out.println();
+        System.out.println("inputArray3: " + Arrays.toString(inputArray3));
+        System.out.println("outputArray3: " + Arrays.toString(outputArray3));
+        System.out.println("true - бонус выше 5000 ₽, false - бонус 5000 ₽ или ниже");
 
         System.out.println("\n=== Задача 4 ===");
-        // Задача 4: Преобразование нечетных чисел в четные
-        System.out.println("Исходный массив: " + Arrays.toString(intArray));
+        // Задача 4: Проверка на просрочки платежей
+        int[] inputArray4 = {1500, -200, 300, 1200, 800}; // Один отрицательный баланс
+        // int[] inputArray4 = {1500, 200, 300, 1200, 800}; // Все положительные балансы
 
-        // Проходим по массиву и делаем все нечетные числа четными
-        for (int i = 0; i < intArray.length; i++) {
-            if (intArray[i] % 2 != 0) { // Если число нечетное
-                intArray[i] += 1; // Делаем четным
+        boolean outputArray4 = true; // Предполагаем, что просрочек нет
+
+        for (int balance : inputArray4) {
+            if (balance < 0) {
+                outputArray4 = false;
+                break; // Прерываем проверку при первой просрочке
             }
         }
 
-        System.out.println("Преобразованный массив: " + Arrays.toString(intArray));
+        System.out.println("inputArray4: " + Arrays.toString(inputArray4));
+        System.out.println("outputArray4: " + outputArray4);
+        System.out.println("true - просрочек нет, false - есть просрочки");
+
+        System.out.println("\n=== Задача 5 ===");
+        // Задача 5: Подсчет рентабельных месяцев
+        int[] inputArray5 = {150000, -50000, 200000, -10000, 180000};
+        int profitableMonths = 0;
+
+        for (int profit : inputArray5) {
+            if (profit > 0) {
+                profitableMonths++;
+            }
+        }
+
+        int[] outputArray5 = {profitableMonths};
+
+        System.out.println("inputArray5: " + Arrays.toString(inputArray5));
+        System.out.println("outputArray5: " + Arrays.toString(outputArray5));
+        System.out.println("Количество рентабельных месяцев (прибыль > 0): " + profitableMonths);
     }
 }
